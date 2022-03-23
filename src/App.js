f5f102a23f150ce77322038193f85bcd7fba2089
lalitkumar22 <66874365+lalitkumar22@users.noreply.github.com>
2022-03-23 12:42:48 +0530

    new thing
    its take old data then now i change

--- src/App.js
+++ src/App.js
@@ -1,25 +1,76 @@
-import logo from './logo.svg';
+import React, { useState,useEffect } from "react";
 import './App.css';
 
-function App() {
+const App = () => {
+  const[counter, setCounter] = useState(0)
+const useEffectAPI = () =>{
+  const App= async() => {
+  const response = await fetch('https://gorest.co.in/public/v1/todos');
+  const data = await response.json();
+  console.log(data);
+}
+useEffect(() => {
+  App();
+});
+
+}
+    const handleClick1 = () => {
+    setCounter(counter + 3)
+  }
+  const handleClick2 = () => {
+    setCounter(counter -3)
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{ 
+      display: 'flex',
+      flexDirection: 'column',
+      alignItems: 'center',
+      justifyContent: "center",
+      fontsize: '400%',
+      position: 'absolute',
+      width: '100%',
+      height: '100%',
+      top: '-15%'
+    }}>
+    Counter App
+    <div style ={{
+      fontSize: '400%',
+      position: 'relative',
+      top: '10vh',
+    }}>
+      {counter}
+
     </div>
-  );
+    <div className="buttons">
+    <button style={{
+      fontSize: '100%',
+      position: 'relative',
+      top: '20vh',
+      marginRight: '5px',
+      backgroundColor: 'green',
+      borderRadius: '8%',
+      color: 'white',
+     }}
+     onClick={handleClick1}>Increment</button>
+     <button style= {{
+       fontSize: '100%',
+       position: 'relative',
+       top: '20vh',
+       marginLeft: '5px',
+       backgroundColor: 'red',
+       borderRadius: '8%',
+       color: 'white', 
+     }}
+     
+     onClick={handleClick2}>Decrement</button>
+     </div>
+     </div>
+      
+  )
 }
 
 export default App;
+
+
+
